de53f2138d18b1eadf83424304eff6f2b79c6ea2
carlos <juniorcastiglioni@gmail.com>
2022-03-16 17:07:44 -0300

    select function finished and favorite button created

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -16,42 +16,76 @@ class _HomePageState extends State<HomePage> {
   NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
   List<Coin> selected = [];
 
+  dynamicAppbar() {
+    if (selected.isEmpty) {
+      return AppBar(
+        title: Center(child: Text("Cryptocurrencies")),
+      );
+    } else {
+      return AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              selected = [];
+            });
+          },
+        ),
+        title: Center(child: Text('${selected.length} selected')),
+        backgroundColor: Colors.blueGrey[50],
+        elevation: 1,
+        iconTheme: IconThemeData(color: Colors.black87),
+        titleTextStyle: TextStyle(
+            color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Cryptocurrencies")),
-        ),
-        body: ListView.separated(
-            itemBuilder: (BuildContext context, int coin) {
-              return ListTile(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12))),
-                leading: (selected.contains(table[coin]))
-                    ? CircleAvatar(
-                        child: Icon(Icons.check),
-                      )
-                    : SizedBox(width: 40, child: Image.asset(table[coin].icon)),
-                title: Text(table[coin].name,
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.w500,
-                    )),
-                trailing: Text(real.format(table[coin].price)),
-                selected: selected.contains(table[coin]),
-                selectedTileColor: Colors.indigo[50],
-                onLongPress: () {
-                  setState(() {
-                    (selected.contains(table[coin]))
-                        ? selected.remove(table[coin])
-                        : selected.add(table[coin]);
-                    print(table[coin].name);
-                  });
-                },
-              );
-            },
-            padding: EdgeInsets.all(16),
-            separatorBuilder: (_, __) => Divider(),
-            itemCount: table.length));
+      appBar: dynamicAppbar(),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int coin) {
+            return ListTile(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
+              leading: (selected.contains(table[coin]))
+                  ? CircleAvatar(
+                      child: Icon(Icons.check),
+                    )
+                  : SizedBox(width: 40, child: Image.asset(table[coin].icon)),
+              title: Text(table[coin].name,
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                  )),
+              trailing: Text(real.format(table[coin].price)),
+              selected: selected.contains(table[coin]),
+              selectedTileColor: Colors.indigo[50],
+              onLongPress: () {
+                setState(() {
+                  (selected.contains(table[coin]))
+                      ? selected.remove(table[coin])
+                      : selected.add(table[coin]);
+                  print(table[coin].name);
+                });
+              },
+            );
+          },
+          padding: EdgeInsets.all(16),
+          separatorBuilder: (_, __) => Divider(),
+          itemCount: table.length),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: selected.isNotEmpty
+          ? FloatingActionButton.extended(
+              onPressed: () {},
+              icon: Icon(Icons.star),
+              label: Text(
+                "Favorite",
+                style: TextStyle(letterSpacing: 0, fontWeight: FontWeight.bold),
+              ))
+          : null,
+    );
   }
 }
