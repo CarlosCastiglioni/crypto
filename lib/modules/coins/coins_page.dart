f3ff0144d050291cf113e4ec693b8e952a8d9833
carlos <juniorcastiglioni@gmail.com>
2022-03-17 12:24:57 -0300

    creating navigation bar, home page and favorite page

---
+++ lib/modules/coins/coins_page.dart
@@ -0,0 +1,97 @@
+import 'package:crypto/modules/coin_details/coin_details_page.dart';
+import 'package:crypto/repositories/coin.repository.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../models/coin.dart';
+
+class CoinsPage extends StatefulWidget {
+  const CoinsPage({Key? key}) : super(key: key);
+
+  @override
+  State<CoinsPage> createState() => _CoinsPageState();
+}
+
+class _CoinsPageState extends State<CoinsPage> {
+  final table = CoinRepository.table;
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  List<Coin> selected = [];
+
+  dynamicAppbar() {
+    if (selected.isEmpty) {
+      return AppBar(
+        title: Center(child: Text("Cryptocurrencies")),
+      );
+    } else {
+      return AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              selected = [];
+            });
+          },
+        ),
+        title: Center(child: Text('${selected.length} selected')),
+        backgroundColor: Colors.blueGrey[50],
+        elevation: 1,
+        iconTheme: IconThemeData(color: Colors.black87),
+        titleTextStyle: TextStyle(
+            color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),
+      );
+    }
+  }
+
+  showDetails(Coin coin) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (_) => CoinDetailsPage(coin: coin)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: dynamicAppbar(),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int coin) {
+            return ListTile(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
+              leading: (selected.contains(table[coin]))
+                  ? CircleAvatar(
+                      child: Icon(Icons.check),
+                    )
+                  : SizedBox(width: 40, child: Image.asset(table[coin].icon)),
+              title: Text(table[coin].name,
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                  )),
+              trailing: Text(real.format(table[coin].price)),
+              selected: selected.contains(table[coin]),
+              selectedTileColor: Colors.indigo[50],
+              onLongPress: () {
+                setState(() {
+                  (selected.contains(table[coin]))
+                      ? selected.remove(table[coin])
+                      : selected.add(table[coin]);
+                });
+              },
+              onTap: () => showDetails(table[coin]),
+            );
+          },
+          padding: EdgeInsets.all(16),
+          separatorBuilder: (_, __) => Divider(),
+          itemCount: table.length),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: selected.isNotEmpty
+          ? FloatingActionButton.extended(
+              onPressed: () {},
+              icon: Icon(Icons.star),
+              label: Text(
+                "Favorite",
+                style: TextStyle(letterSpacing: 0, fontWeight: FontWeight.bold),
+              ))
+          : null,
+    );
+  }
+}
--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,9 +1,7 @@
-import 'package:crypto/modules/coin_details/coin_details_page.dart';
-import 'package:crypto/repositories/coin.repository.dart';
+import 'package:crypto/modules/coins/coins_page.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-import '../../models/coin.dart';
+import '../favorites/favorites_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,85 +11,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final table = CoinRepository.table;
-  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
-  List<Coin> selected = [];
+  int currentPage = 0;
+  late PageController pc;
 
-  dynamicAppbar() {
-    if (selected.isEmpty) {
-      return AppBar(
-        title: Center(child: Text("Cryptocurrencies")),
-      );
-    } else {
-      return AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            setState(() {
-              selected = [];
-            });
-          },
-        ),
-        title: Center(child: Text('${selected.length} selected')),
-        backgroundColor: Colors.blueGrey[50],
-        elevation: 1,
-        iconTheme: IconThemeData(color: Colors.black87),
-        titleTextStyle: TextStyle(
-            color: Colors.black87, fontSize: 20, fontWeight: FontWeight.bold),
-      );
-    }
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: currentPage);
   }
 
-  showDetails(Coin coin) {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (_) => CoinDetailsPage(coin: coin)));
+  setPage(page) {
+    setState(() {
+      currentPage = page;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: dynamicAppbar(),
-      body: ListView.separated(
-          itemBuilder: (BuildContext context, int coin) {
-            return ListTile(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12))),
-              leading: (selected.contains(table[coin]))
-                  ? CircleAvatar(
-                      child: Icon(Icons.check),
-                    )
-                  : SizedBox(width: 40, child: Image.asset(table[coin].icon)),
-              title: Text(table[coin].name,
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.w500,
-                  )),
-              trailing: Text(real.format(table[coin].price)),
-              selected: selected.contains(table[coin]),
-              selectedTileColor: Colors.indigo[50],
-              onLongPress: () {
-                setState(() {
-                  (selected.contains(table[coin]))
-                      ? selected.remove(table[coin])
-                      : selected.add(table[coin]);
-                });
-              },
-              onTap: () => showDetails(table[coin]),
-            );
-          },
-          padding: EdgeInsets.all(16),
-          separatorBuilder: (_, __) => Divider(),
-          itemCount: table.length),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: selected.isNotEmpty
-          ? FloatingActionButton.extended(
-              onPressed: () {},
-              icon: Icon(Icons.star),
-              label: Text(
-                "Favorite",
-                style: TextStyle(letterSpacing: 0, fontWeight: FontWeight.bold),
-              ))
-          : null,
+      body: PageView(
+        onPageChanged: setPage,
+        controller: pc,
+        children: [CoinsPage(), FavoritesPage()],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: "All"),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites")
+        ],
+        currentIndex: currentPage,
+        onTap: (page) {
+          pc.animateToPage(page,
+              duration: Duration(milliseconds: 400), curve: Curves.ease);
+        },
+        backgroundColor: Colors.grey[100],
+      ),
     );
   }
 }
